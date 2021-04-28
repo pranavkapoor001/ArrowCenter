81ec994ae54c5e6232ffcd8a1a61faec096b631e
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-04-28 20:00:43 +0530

    ArrowCenter: Add swipe to remove notification
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- app/src/main/java/com/pk/arrowcenter/ui/activities/MainActivity.java
+++ app/src/main/java/com/pk/arrowcenter/ui/activities/MainActivity.java
@@ -2,19 +2,25 @@ package com.pk.arrowcenter.ui.activities;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.pk.arrowcenter.R;
 import com.pk.arrowcenter.Utils.FirebaseUtils;
 import com.pk.arrowcenter.Utils.NotificationDataUtils;
+import com.pk.arrowcenter.models.NotificationModel;
 import com.pk.arrowcenter.ui.adapters.NotificationAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     // Vars
     private RecyclerView recyclerView;
+    private NotificationDataUtils dataUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize recycler view
         initNotificationList();
+
+        // Initialize notification item swipe listener
+        initTouchHelper();
     }
 
     /**
@@ -57,9 +66,40 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         // Get data
-        NotificationDataUtils dataUtils = new NotificationDataUtils(this);
+        dataUtils = new NotificationDataUtils(this);
 
         NotificationAdapter notificationAdapter = new NotificationAdapter(dataUtils.getSavedNotifications());
         recyclerView.setAdapter(notificationAdapter);
+    }
+
+
+    /**
+     * Updates notification list on swipe gesture
+     *
+     * @param position index of swiped item
+     */
+    private void handleOnSwiped(int position) {
+        ArrayList<NotificationModel> notificationsList = dataUtils.getSavedNotifications();
+        notificationsList.remove(position);
+        dataUtils.updateNotificationList(notificationsList);
+    }
+
+    /**
+     * Registers item move and swipe listener on {@link NotificationAdapter}
+     */
+    private void initTouchHelper() {
+        ItemTouchHelper touchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                handleOnSwiped(viewHolder.getAdapterPosition());
+            }
+        });
+        touchHelper.attachToRecyclerView(recyclerView);
     }
 }
